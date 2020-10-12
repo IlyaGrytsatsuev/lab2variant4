7b7d61fc287420d14e0cd6039f6d4693f6f9072e
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2020-10-12 20:33:49 +0300

    initial

--- lab2variant4/main.c
+++ lab2variant4/main.c
@@ -5,11 +5,34 @@
 //  Created by gru gri on 21.09.2020.
 //  Copyright © 2020 gru gri. All rights reserved.
 //
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main ()
+
+{
+int c=0;
+
+    int num_words=0, state = 0, num_chars = 0, k ;
+    printf("Enter the length: ");
+    scanf("%d",&k);
+    printf("Enter the sentence:\n");
+   while ((c=getchar()) != EOF && c != '\n')
+{
+
+    if (('a'<=c&&c<='z')||('A'<=c&&c<='Z'))
+state = 1;
+    if (('a'<=c&&c<='z')||('A'<=c&&c<='Z'))
+        num_chars++;
+    else {
+        if(state == 1 && num_chars==k)
+        num_words++;
+        state = 0;
+        num_chars = 0;
+    }
+    }
+    if (state == 1&&num_chars==k)
+        num_words ++;
+    num_chars = 0;
+printf("Number of words: %d\n",num_words);
+return 0;
 }
