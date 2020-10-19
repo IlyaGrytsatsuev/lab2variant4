e1488b8ebe3df4f51fb3c243b2053e719067ecbb
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2020-10-19 11:01:55 +0300

    formating

--- lab2variant4/main.c
+++ lab2variant4/main.c
@@ -6,33 +6,36 @@
 //  Copyright © 2020 gru gri. All rights reserved.
 //
 #include <stdio.h>
-
 int main ()
-
 {
-int c=0;
-
+    char c;
     int num_words=0, state = 0, num_chars = 0, k ;
     printf("Enter the length: ");
     scanf("%d",&k);
-    printf("Enter the sentence and type'=' to finish the program:\n");
-   while ((c=getchar()) != EOF && c != '=')
-{
-
-    if (('a'<=c&&c<='z')||('A'<=c&&c<='Z'))
-state = 1;
-    if (('a'<=c&&c<='z')||('A'<=c&&c<='Z'))
-        num_chars++;
-    else {
-        if(state == 1 && num_chars==k)
-        num_words++;
-        state = 0;
-        num_chars = 0;
+    if (k == 0 || k < 0){
+        printf(" there are no such words\n");
+        return 0;
     }
+    printf("Enter the sentence:\n	");
+    getchar();
+    while (((c = getchar()) != EOF) && (c != '\n'))
+    {
+        if (('a' <= c &&c <= 'z')||('A' <= c && c <= 'Z')){
+            state = 1;
+            num_chars++;
+        }
+        else {
+            if(state == 1 && num_chars == k)
+                num_words++;
+            state = 0;
+            num_chars = 0;
+        }
     }
-    if (state == 1&&num_chars==k)
+    if (state == 1 && num_chars == k){
         num_words ++;
     num_chars = 0;
-printf("Number of words: %d\n",num_words);
-return 0;
+    printf("Number of words: %d\n",num_words);
+    return 0;
+    }
 }
+
